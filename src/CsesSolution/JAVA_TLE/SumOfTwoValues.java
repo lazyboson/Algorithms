bbd87a477425295a77ab3966bb5bf5d80f56d18f
Ashutosh Pandey <63603603+lazyboson@users.noreply.github.com>
2021-11-07 20:48:16 +0530

    running commit

--- src/CsesSolution/JAVA_TLE/SumOfTwoValues.java
+++ src/CsesSolution/JAVA_TLE/SumOfTwoValues.java
@@ -3,10 +3,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.PrintStream;
+import java.util.TreeMap;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
- 
+
 /**
  * Built using CHelper plug-in
  * Actual solution is at the top
@@ -19,23 +19,38 @@ public class Main {
         OutputStream outputStream = System.out;
         Scanner in = new Scanner(inputStream);
         PrintWriter out = new PrintWriter(outputStream);
-        DistinctNumbers solver = new DistinctNumbers();
+        SumOfTwoValues solver = new SumOfTwoValues();
         solver.solve(1, in, out);
         out.close();
     }
- 
-    static class DistinctNumbers {
+
+    static class SumOfTwoValues {
         public void solve(int testNumber, Scanner in, PrintWriter out) {
+            int[] ans = new int[2];
             int n;
-            Set<Long> s = new TreeSet<>();
             n = in.nextInt();
+            long sum;
+            sum = in.nextInt();
+            long[] arr = new long[n];
             for (int i = 0; i < n; i++) {
-                long input;
-                input = in.nextLong();
-                s.add(input);
+                arr[i] = in.nextLong();
             }
-            System.out.println(s.size());
+            Map<Long, Integer> m = new TreeMap<>();
+            for (int i = 0; i < n; i++) {
+                if (!m.containsKey(sum - arr[i])) {
+                    m.put(arr[i], i + 1);
+                } else {
+                    ans[0] = m.get(sum - arr[i]);
+                    ans[1] = i + 1;
+                    break;
+                }
+            }
+            if (ans[0] == 0 && ans[1] == 0)
+                System.out.println("IMPOSSIBLE");
+            else
+                System.out.printf(ans[0] + " " + ans[1]);
         }
- 
+
     }
 }
+
